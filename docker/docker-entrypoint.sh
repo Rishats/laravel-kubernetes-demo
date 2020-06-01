a4417109043fcdb601a763ac1e207194d7a121ef
Rishats <sultanov-rishat@inbox.ru>
2020-06-01 16:57:43 +0600

    Redis Master + Slaves added + configmal.yaml created

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,9 +7,7 @@ ENV_FILE="/var/www/html/.env"
 . ~/.bashrc
 
 # ACL for permissions
-setfacl -R -m u:nginx:rwx /var/www
 setfacl -R -m u:www-data:rwx /var/www
-setfacl -R -m g:nginx:rwx /var/www
 setfacl -R -m g:www-data:rwx /var/www
 
 # StartUp Nginx and PHP-FPM
